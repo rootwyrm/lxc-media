9d17c40afd362483497834c4b812eb2baef34229
Phillip R. Jaenke <prj@rootwyrm.com>
2016-02-14 14:33:44 +0000

    SABnzbd complete.. may need to add more layers so initial deploy is faster.

--- sabnzbd/README.md
+++ sabnzbd/README.md
@@ -1,2 +1,7 @@
-docker create -p 9080:9080 -p 9081:9081 -v /tmp/testsab:/config -v /tmp/download:/downloads --name=testsab alpsab
+# Known Issues
+* https port interferes with cockpit
+* first run script displays three times after setup (cosmetic)
+
+# Sample Setup
+docker create -p 9080:9080 -p 9090:9090 -v /media/config/sabnzbd:/config -v /media/download:/downloads --name=testsab docker.io/rootwyrm/lxc-media:sabnzbd
 
--- sabnzbd/application/deploy.sh
+++ sabnzbd/application/deploy.sh
@@ -1,258 +1,117 @@
 #!/bin/bash
-# application/deploy.sh
-# vers = 4c5e8caddcb7eca6115862d2fd4f29c2
 
-chkfile="/deploy"
-basedir="/opt/rootwyrm/defaults"
+# Copyright (C) 2015-* Phillip R. Jaenke <docker@rootwyrm.com>
+#
+# NO COMMERCIAL REDISTRIBUTION IN ANY FORM IS PERMISSIBLE WITHOUT EXPRESS
+# WRITTEN CONSENT FROM PHILLIP R. JAENKE. IGNORANCE IS NOT A DEFENSE.
+# SEE /LICENSE
 
-app_name="sabnzbd"
-app_git_url="https://github.com/sabnzbd/sabnzbd.git"
-app_destdir="/opt/sabnzbd"
+export app_name="sabnzbd"
+export app_url="http://www.sabnzbd.org/"
+export app_git_url="https://github.com/sabnzbd/sabnzbd.git"
+export app_destdir="/opt/sabnzbd"
 
-## First things first, stop application
-# Trap early
-test_run()
+#export app_cmd="/usr/bin/python /opt/sabnzbd/SABnzbd.py"
+export app_svname="sabnzbd"
+export app_psname="SABnzbd.py"
+
+## Ingest library
+. /opt/rootwyrm/deploy.lib.sh
+
+sabnzbd_deploy_config()
 {
-	if [ ! -f $chkfile ]; then
-		exit 0
+	if [ ! -f /config/sabnzbd.ini ]; then
+		cp /opt/rootwyrm/defaults/sabnzbd.ini /config
+		export sabnzbd_regen_api="true"
 	else
-		# Try to ingest deploy configuration
-		if [ -f $chkfile ]; then
-			. $chkfile
-		elif [ -f "$chkfile".conf ]; then
-			. "$chkfile".conf
+		echo "[NOTICE] Not overwriting existing sabnzbd.ini"
+	fi
+	chown $lxcuser:$lxcgroup /config/sabnzbd.ini
+
+	if [ ! -f /config/openssl.cnf ]; then
+		cp /opt/rootwyrm/defaults/openssl.cnf /config
+	else
+		echo "[NOTICE] Not overwriting existing openssl.cnf"
+	fi
+	chown $lxcuser:$lxcgroup /config/openssl.cnf
+}
+
+sabnzbd_config_checkdir()
+{
+	test -d /downloads
+	if [ $? -ne 0 ]; then
+		echo "[FATAL] /downloads is missing!!"
+		exit 1
+	fi
+	chown $lxcuser:$lxcgroup /downloads
+
+	test -d /config
+	if [ $? -ne 0 ]; then
+		echo "[FATAL] /config is missing!!"
+		exit 1
+	fi
+	if [[ $(stat -c %U /config) != $lxcuser ]]; then
+		chown $lxcuser /config
+	fi
+	if [[ $(stat -c %G /config) != $lxcgroup ]]; then
+		chgrp $lxcgroup /config
+	fi
+
+	if [ ! -f /config/sabnzbd.ini ]; then
+		## Try to reinstall from defaults
+		cp /opt/rootwyrm/defaults/sabnzbd.ini /config/sabnzbd.ini
+		if [ $? -ne 0 ]; then
+			echo "[FATAL] Missing /config/sabnzbd.ini and could not replace!"
+			exit 1
 		fi
+		chown $lxcuser:$lxcgroup /config/sabnzbd.ini
 	fi
-}
 
-## Generic error check
-check_error()
-{
-	if [ $1 -ne 0 ]; then
-		RC=$1
-		if [ -z $2 ]; then
-			echo "[FATAL] Error occurred in $2"
-			exit $RC
-		else
-			echo "[FATAL] Error occurred in $2 : $1"
-			exit $RC
-		fi
-	fi
-}
-
-## Clean up supervisord so it doesn't re-run deploy
-supervisord_clean()
-{
-	if [ -f /etc/supervisor.d/deploy.ini ]; then
-		mv /etc/supervisor.d/deploy.ini /etc/supervisor.d/deploy.done
-	fi
-}
-
-
-deploy_app_config()
-{
 	if [ -f /config/sabnzbd.ini ]; then
-		echo "[WARNING] Overwriting existing configuration."
-		mv /config/sabnzbd.ini /config/sabnzbd.ini.$(date "+%d%m%Y")
-		cp $basedir/sabnzbd.ini /config/sabnzbd.ini
-		check_error $? config_ini
-		chown $lxcuid:$lxcgid /config/sabnzbd.ini
-	else
-		cp $basedir/sabnzbd.ini /config/sabnzbd.ini
-		check_error $? config_ini
-		chown $lxcuid:$lxcgid /config/sabnzbd.ini
+		for cfgdir in `cat /config/sabnzbd.ini | grep "^dir =" | awk '{print $2}'`; do
+			if [ -d $cfgdir ]; then
+				if [[ `stat -c %U $cfgdir` != $lxcuser ]]; then
+					chown $lxcuser $cfgdir
+				fi
+				if [[ `stat -c %G $cfgdir` != $lxcgroup ]]; then
+					chgrp $lxcgroup $cfgdir
+				fi
+			fi
+		done
+	fi
+	## Check blackhole
+	blackhole_dir=$(grep dirscan_dir /config/sabnzbd.ini | awk '{print $2}')
+	if [ ! -d $blackhole_dir ] && [ -z $blackhole_dir ]; then
+		mkdir $blackhole_dir
+		chown $lxcuser:$lxcgroup $blackhole_dir
+	fi
+	## Incomplete Directory
+	incomp_dir=$(grep incomplete_dir /config/sabnzbd.ini | awk '{print $2}')
+	if [ ! -d $incomp_dir ] && [ -z $incomp_dir ]; then
+		mkdir $incomp_dir
+		chown $lxcuser:$lxcgroup $incomp_dir
 	fi
 
-	## Check for required directories.
-	if [ ! -d /config/nzb ]; then
-		mkdir /config/nzb ; chown $lxcuid:$lxcgid /config/nzb
-	fi
-	if [ ! -d /config/email ]; then
-		mkdir /config/email ; chown $lxcuid:$lxcgid /config/email
-	fi
-	if [ ! -d /config/admin ]; then
-		mkdir /config/admin ; chown $lxcuid:$lxcgid /config/admin
-	fi
-	if [ ! -d /config/logs ]; then
-		mkdir /config/logs ; chown $lxcuid:$lxcgid /config/logs
-	fi
+	## SSL Directory 
 	if [ ! -d /config/ssl ]; then
-		mkdir /config/ssl ; chown $lxcuid:$lxcgid /config/ssl
-	fi
-
-	if [[ $(stat -c %u /config) != $lxcuid ]]; then
-		chown $lxcuid /config
-	fi
-	if [[ $(stat -c %g /config) != $lxcgid ]]; then
-		chgrp $lxcgid /config
-	fi
-
-	## Download directories
-	for dest in complete incomplete television movies music comics books; do
-		if [ ! -d /downloads/$dest ]; then
-			mkdir /downloads/$dest ; chown $lxcuid:$lxcgid /downloads/$dest
-		fi
-	done
-	if [[ $(stat -c %a /downloads) -lt 777 ]]; then
-		chmod 0777 /downloads
-	fi
-
-	## Move the supervisor configuration to active.
-	mv /etc/supervisor.d/sabnzbd.holdini /etc/supervisor.d/sabnzbd.ini
-}
-
-## Create the user here, cuts down on layers.
-deploy_lxcmedia_user()
-{
-	## If we're on a Synology, we run as DSM admin uid, user gid.
-	if [[ -f /proc/syno_cpu_arch ]]; then
-		export lxcuser="lxcmedia"
-		export lxcuid="1024"
-		export lxcgroup="users"
-		export lxcgid="100"
+		mkdir /config/ssl
+		chown $lxcuser:$lxcgroup /config/ssl
+		chmod 0700 /config/ssl
 	else
-		# Defaults are lxcmedia:users @ 1024:100
-		if [[ -z $lxcuser ]]; then export lxcuser="lxcmedia"; fi
-		if [[ -z $lxcuid ]]; then export lxcuid="1024"; fi
-		if [[ -z $lxcgroup ]]; then export lxcgroup="users"; fi
-		if [[ -z $lxcgid ]]; then 
-			grep $lxcgroup /etc/group > /dev/null
-			if [ $? -ne 0 ]; then
-				set lxcgid="100"
-				addgroup -g $lxcgid $lxcgroup
-				check_error $? addgroup
-			else
-				grep $lxcgroup /etc/group | cut -d : -f 3 > /tmp/gid
-				export lxcgid=$(cat /tmp/gid)
-			fi
+		if [[ `stat -c %U /config/ssl` != $lxcuser ]]; then
+			chown -R $lxcuser /config/ssl
+		fi
+		if [[ `stat -c %G /config/ssl` != $lxcgroup ]]; then
+			chgrp -R $lxcgroup /config/ssl
+		fi
+		if [[ `stat -c %a /config/ssl` != '700' ]]; then
+			chmod -R 0700 /config/ssl
 		fi
 	fi
-	if [ -z $lxcshell ]; then
-		## XXX: gliderlabs/docker-alpine/issues/141
-		export lxcshell=/bin/sh
-	fi
 
-	if [[ $(id -u $lxcuser) -eq $lxcuid ]]; then
-		return 0
-	elif [[ $(id -u $lxcuser) != 0 ]]; then
-		## NOP
-		echo -n "" > /dev/null
-	else
-		deluser $lxcuser
-	fi
-	
-	# NOTE: NEVER use \ to make readable, base chokes on it.	
-	adduser -h /home/$lxcuser -g "RootWyrm Media Compose User" -u $lxcuid -G $lxcgroup -D -s $lxcshell $lxcuser
-	check_error $? adduser
-
-	rm /tmp/gid
 }
 
-supervisor_setowner()
-{
-	sed -i -e 's,_LXCUSER,'$lxcuser',g' /etc/supervisord.conf ; \
-	sed -i -e 's,_LXCUSER,'$lxcuser',g' /etc/supervisor.d/*.ini ; \
-	sed -i -e 's,_LXCGROUP,'$lxcgroup',g' /etc/supervisord.conf ; \
-	sed -i -e 's,_LXCGROUP,'$lxcgroup',g' /etc/supervisor.d/*.ini
-}
-
-## Set ownerships
-deploy_lxcmedia_ownership()
-{
-	## XXX: numeric to work around Synology issues. May be fixed in
-	## DSM6 but not holding my breath.
-	if [[ -z $lxcuid ]] || [[ -z $lxcgid ]]; then
-		printf '[FATAL] $lxcuid or $lxcgid unset!\n'
-		return 1
-	fi
-	
-	chown -R $lxcuid:$lxcgid /home/$lxcuser
-	check_error $? chown_home
-	chmod 0700 /home/$lxcuser
-	chown -R $lxcuid:$lxcgid /config
-	check_error $? chown_config
-
-	chown -R $lxcuid:$lxcgid $app_destdir
-	check_error $? chown_app_destdir
-	# Don't forget the git files...
-	chown -R $lxcuid:$lxcgid $app_destdir/.[a-z]*
-	check_error $? chown_app_destdir
-}
-
-deploy_application_git()
-{
-	## This function generally shouldn't come up, but might.
-	case $1 in
-		[rR][eE][iI][nN][sS][tT]*)
-			if [[ -z $app_destdir ]] && [[ -d $app_destdir ]]; then
-				rm -rf $app_destdir
-				mkdir $app_destdir
-			fi
-			git clone $app_git_url -b master --depth=1 $app_destdir
-			check_error $? git_clone
-			chown -R $lxcuid:$lxcgid $app_destdir
-			return $?
-			;;
-		[uU][pP][dD][aA][tT][eE])
-			if [ ! -d $app_destdir ]; then
-				## Presume user error.
-				deploy_application_git REINST
-			else
-				export return=$PWD; cd $app_destdir
-				su $lxcuser -c 'git pull'
-				check_error $? git_pull_update
-			   	cd $return
-				unset return
-			fi
-			return $?
-			;;
-	esac
-}
-
-ssl_ssc_create()
-{
-	export ssldir="/config/ssl"
-	if [[ -f $ssldir/media.crt ]] || [[ -f $ssldir/media.key ]]; then
-		## Don't obliterate user provided key.
-		return 0
-	fi
-
-	export sslpass=$(head -c 500 /dev/urandom | tr -dc a-z0-9A-Z | head -c 64)
-	export mydomain=$(cat /etc/resolv.conf | grep search | awk '{print $2}')
-	export myhost=$(cat /etc/hostname)
-	## Self-signed certs use the docker container ID and domain.
-	export OPENSSLCONFIG=/opt/rootwyrm/defaults/openssl.cnf
-	## Fix-up openssl.cnf
-	sed -i -e 's,_REPLACE_HOSTNAME_,'$myhost'.'$mydomain',' $OPENSSLCONFIG
-	sed -i -e 's,_DOMAIN_,'$mydomain',' $OPENSSLCONFIG
-
-	## genkey
-	openssl genrsa -des3 -out $ssldir/media.key -passout env:sslpass 2048
-	check_error $? ssl_gen_key
-
-	## gencsr
-	openssl req -new -x509 -days 3650 -batch -nodes \
-		-config $OPENSSLCONFIG -key $ssldir/media.key \
-		-out $ssldir/media.crt -passin env:sslpass
-	check_error $? ssl_gen_csr
-
-	mv $ssldir/media.key $ssldir/media.key.lock
-
-	## unlock key
-	openssl rsa -in $ssldir/media.key.lock -out $ssldir/media.key -passin env:sslpass 
-	check_error $? ssl_unlock_key
-
-	## gencert
-	#openssl x509 -req -days 3650 -in $ssldir/media.csr -signkey $ssldir/media.key -out $ssldir/media.crt -config $OPENSSLCONFIG
-	#check_error $? ssl_gen_cert
-
-	for sslfile in `ls $ssldir/*`; do
-		chown $lxcuid:$lxcgid $sslfile
-		chmod 0600 $sslfile
-	done
-}
-
-sabnzbd_regen_api()
+sabnzbd_regen_apikey()
 {
 	call="api?mode=config&name=set_apikey&apikey=357e729fed6617e4cabd237a36179f00"
 	## sabnzbd doesn't listen on localhost.
@@ -265,47 +124,56 @@ sabnzbd_regen_api()
 	chown $lxcuid:$lxcgid /config/sabnzbd.api
 }
 
-## Mark as complete
-deploy_complete()
-{
-	if [ -f $chkfile ]; then
-		rm $chkfile
-	fi
-	if [ -f /deploy.new ]; then
-		rm /deploy.new
-	fi
-}
 
-test_run
+######################################################################
+## execution phase
+######################################################################
 
-. /.dockerenv 
+## XXX: Bugfix
+ln -s /etc/service /service
 
-## Extra clean-up bit.
-if [ -f /root/pip.py ]; then
-	rm /root/pip.py
+ingest_environment
+test_deploy
+
+generate_motd
+cat /etc/motd
+
+## Do build phase.
+for bp in `ls /root/build/*.sh | sort`; do
+	chmod +x $bp
+	$bp
+	check_error $? $bp
+done
+
+echo "[DEPLOY] Deploying lxc-media user:"
+deploy_lxcmedia_user
+
+deploy_application_git reinst
+chown -R $lxcuser:$lxcgroup $app_destdir
+sabnzbd_deploy_config
+
+sabnzbd_config_checkdir
+sabnzbd_deploy_config
+ssl_ssc_create
+
+echo "[DEPLOY] Changing ownerships for lxc-media..."
+deploy_lxcmedia_ownership
+
+runit_linksv $appname
+if [ ! -z $sabnzbd_regen_api ]; then
+	echo "[SABnzbd] Regenerating API Key."
+	sv start sabnzbd; sleep 5; sabnzbd_regen_apikey
+	#while true; do
+	#	sleep 1
+	#	ps ax |grep -i sabnzbd > /dev/null
+	#	if [ $? -ne 0 ]; then
+	#		break
+	#	fi
+	#done
+	sv restart sabnzbd
+	#sleep 10
+	#sabnzbd_regen_apikey
+	#sv stop sabnzbd
 fi
 
-if [ -z $1 ]; then
-	## We're at zero
-	deploy_lxcmedia_user
-	deploy_app_config
-	deploy_lxcmedia_ownership
-	supervisor_setowner
-	ssl_ssc_create
-	## Prevent supervisord from reaping deploy..	
-	supervisord_clean	
-	/usr/bin/supervisorctl reload
-	sleep 5
-	/usr/bin/supervisorctl start sabnzbd
-	## sabnzbd takes a while to start
-	sleep 30
-	sabnzbd_regen_api
-	deploy_complete		## Because we don't want it doing stupid.
-else
-	/usr/bin/supervisorctl stop sabnzbd
-	deploy_application_git $1
-	deploy_lxcmedia_ownership
-	/usr/bin/supervisorctl restart sabnzbd
-fi
-
-exit 0
+deploy_complete
