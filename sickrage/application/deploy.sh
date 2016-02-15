6ea6e497752e2ad46f023a6588af4f850d77f0ef
Phillip R. Jaenke <prj@rootwyrm.com>
2016-02-15 12:33:54 -0500

    SickRage is done, woohoo!

--- sickrage/application/deploy.sh
+++ sickrage/application/deploy.sh
@@ -1,186 +1,120 @@
 #!/bin/bash
-# application/deploy.sh
-# vers = 4c5e8caddcb7eca6115862d2fd4f29c2
+# sickrage/application/deploy.sh
+# vers = 6878a3960a2c24183c9160634d7995f3
 
-chkfile="/deploy"
-basedir="/opt/rootwyrm/defaults"
+# Copyright (C) 2015-* Phillip R. Jaenke <docker@rootwyrm.com>
+#
+# NO COMMERCIAL REDISTRIBUTION IN ANY FORM IS PERMISSIBLE WITHOUT EXPRESS
+# WRITTEN CONSENT FROM PHILLIP R. JAENKE. IGNORANCE IS NOT A DEFENSE.
+# SEE /LICENSE
 
-app_name="sickrage"
-app_git_url="https://github.com/SickRage/SickRage.git"
-app_destdir="/opt/sickrage"
+export app_name="sickrage"
+export app_url="https://sickrage.github.io/"
+export app_git_url="https://github.com/SickRage/SickRage.git"
+export app_destdir="/opt/sickrage"
 
-## First things first, stop application
-/usr/bin/supervisorctl stop $app_name
+#export app_cmd="/usr/bin/python /opt/sickrage/SickBeard.py"
+export app_svname="sickrage"
+export app_psname="SickBeard.py"
 
-# Trap early
-if [ ! -f $chkfile ]; then
-	exit 0
-else
-	# Try to ingest deploy configuration
-	if [ -f $chkfile ]; then
-		. $chkfile
-	elif [ -f "$chkfile".conf ]; then
-		. "$chkfile".conf
-	fi
-fi
+. /opt/rootwyrm/deploy.lib.sh
 
-## Generic error check
-check_error()
+sickrage_deploy_config()
 {
-	if [ $? -ne 0 ]; then
-		RC=$?
-		if [ -z $2 ]; then
-			echo "[FATAL] Error occurred in $1"
-			exit $RC
-		else
-			echo "[FATAL] Error occurred in $1 : $2"
-			exit $RC
-		fi
-	fi
-}
-
-## Clean up supervisord so it doesn't re-run deploy
-supervisord_clean()
-{
-	if [ -f /etc/supervisor.d/deploy.ini ]; then
-		mv /etc/supervisor.d/deploy.ini /etc/supervisor.d/deploy.done
-	fi
-}
-
-## Mark as complete
-deploy_complete()
-{
-	if [ -f /deploy.new ]; then
-		rm /deploy.new
-	fi
-}
-
-deploy_app_config()
-{
-	if [ -f /config/config.ini ]; then
-		if [ -z $DEPLOY_OVERWRITE ]; then
-			printf '[WARNING] Declining to overwrite existing config.\n'
-			return 1
-		else
-			printf '[WARNING] Overwriting existing configuration.\n'
-			mv /config/config.ini /config/config.ini.$(date "+%d%m%Y")
-		fi
-	fi
-	cp $basedir/config.ini /config/
-	check_error config.ini
-}
-
-## Create the user here, cuts down on layers.
-deploy_lxcmedia_user()
-{
-	## If we're on a Synology, we run as DSM admin uid, user gid.
-	if [[ -f /proc/syno_cpu_arch ]]; then
-		export lxcuser="lxcmedia"
-		export lxcuid="1024"
-		export lxcgroup="users"
-		export lxcgid="100"
+	if [ ! -f /config/config.ini ]; then
+		cp /opt/rootwyrm/defaults/config.ini /config
+		echo "[WARNING] You should manually regenerate the API KEY!"
 	else
-		# Defaults are lxcmedia:users @ 1024:100
-		if [[ -z $lkxcuser ]]; then export lxcuser="lkxcmedia"; fi
-		if [[ -z $lxcuid ]]; then export lxcuid="1024"; fi
-		if [[ -z $lxcgroup ]]; then export lxcgroup="users"; fi
-		if [[ -z $lxcgid ]]; then 
-			grep $lxcgroup /etc/group > /dev/null
-			if [ $? -ne 0 ]; then
-				set lxcgid="100"
-				addgroup -g $lxcgid $lxcgroup
-				check_error addgroup
-			else
-				grep $lxcgroup /etc/group | cut -d : -f 3 > /tmp/gid
-				export lxcgid=$(cat /tmp/gid)
-			fi
+		echo "[NOTICE] Not overwriting existing /config/config.ini"
+	fi
+	chown $lxcuser:$lxcgroup /config/config.ini
+
+	if [ ! -f /config/openssl.cnf ]; then
+		cp /opt/rootwyrm/defaults/openssl.cnf /config
+	else
+		echo "[NOTICE] Not overwriting existing /config/openssl.cnf"
+	fi
+	chown $lxcuser:$lxcgroup /config/openssl.cnf
+}
+
+sickrage_config_checkdir()
+{
+	test -d /downloads
+	if [ $? -ne 0 ]; then
+		echo "[FATAL] /downloads is missing!!"
+		exit 1
+	fi
+	chown $lxcuser:$lxcgroup /downloads
+
+	test -d /config
+	if [ $? -ne 0 ]; then
+		echo "[FATAL] /config is missing!!"
+		exit 1
+	fi
+
+	if [[ $(stat -c %U /config) != $lxcuser ]]; then
+		chown $lxcuser /config
+	fi
+	if [[ $(stat -c %G /config) != $lxcgroup ]]; then
+		chgrp $lxcgroup /config
+	fi
+
+	## SSL Directory
+	if [ ! -d /config/ssl ]; then
+		mkdir /config/ssl
+		chown $lxcuser:$lxcgroup /config/ssl
+		chmod 0700 /config/ssl
+	else
+		if [[ `stat -c %U /config/ssl` != $lxcuser ]]; then
+			chown -R $lxcuser /config/ssl
+		fi
+		if [[ `stat -c %G /config/ssl` != $lxcgroup ]]; then
+			chgrp -R $lxcgroup /config/ssl
+		fi
+		if [[ `stat -c %a /config/ssl` != '700' ]]; then
+			chmod -R 0700 /config/ssl
 		fi
 	fi
-	if [ -z $lxcshell ]; then
-		## XXX: gliderlabs/docker-alpine/issues/141
-		export lxcshell=/bin/sh
-	fi
-	
-	# NOTE: NEVER use \ to make readable, base chokes on it.	
-	adduser -h /home/$lxcuser -g "RootWyrm Media Compose User" -u $lxcuid -G $lxcgroup -D -s $lxcshell $lxcuser
-	check_error adduser
-
-	rm /tmp/gid
 }
 
-supervisor_setowner()
+sickrage_regenerate_api()
 {
-	sed -i -e 's,LXCUSER,'$lxcuser',g' /etc/supervisord.conf ; \
-	sed -i -e 's,LXCUSER,'$lxcuser',g' /etc/supervisor.d/*.ini ; \
-	sed -i -e 's,LXCGROUP,'$lxcgroup',g' /etc/supervisord.conf ; \
-	sed -i -e 's,LXCGROUP,'$lxcgroup',g' /etc/supervisor.d/*.ini
+	export NEWAPI=$(/opt/rootwyrm/regenapi.py | cut -d : -f 2)
+	sed -i -e "s,^api_key.*,api_key = $NEWAPI," application/defaults/config.ini
+	echo "[NOTICE] New API Key: $NEWAPI"
 }
 
-## Set ownerships
-deploy_lxcmedia_ownership()
-{
-	## XXX: numeric to work around Synology issues. May be fixed in
-	## DSM6 but not holding my breath.
-	if [[ -z $lxcuid ]] || [[ -z $lxcgid ]]; then
-		printf '[FATAL] $lxcuid or $lxcgid unset!\n'
-		return 1
-	fi
-	
-	chown -R $lxcuid:$lxcgid /home/$lxcuser
-	check_error chown_home
-	chmod 0700 /home/$lxcuser
-	chown -R $lxcuid:$lxcgid /config
-	check_error chown_config
+######################################################################
+## execution phase
+######################################################################
 
-	chown -R $lxcuid:$lxcgid $app_destdir
-	check_error chown_app_destdir
-	# Don't forget the git files...
-	chown -R $lxcuid:$lxcgid $app_destdir/.[a-z]*
-	check_error chown_app_destdir
-}
+ingest_environment
+test_deploy
 
-deploy_application_git()
-{
-	## This function generally shouldn't come up, but might.
-	case $1 in
-		[rR][eE][iI][nN][sS][tT]*)
-			if [[ -z $app_destdir ]] && [[ -d $app_destdir ]]; then
-				rm -rf $app_destdir
-				mkdir $app_destdir
-			fi
-			git clone $app_git_url -b master --depth=1 $app_destdir
-			check_error git_clone
-			return $?
-			;;
-		[uU][pP][dD][aA][tT][eE])
-			if [ ! -d $app_destdir ]; then
-				## Presume user error.
-				deploy_application_git REINST
-			else
-				export return=$PWD; cd $app_destdir
-				git pull
-				check_error git_pull_update
-			   	cd $return
-				unset return
-			fi
-			return $?
-			;;
-	esac
-}
+generate_motd
+cat /etc/motd
 
+## Do build phase.
+for bp in `ls /root/build/*.sh | sort`; do
+	chmod +x $bp
+	$bp
+	check_error $? $bp
+done
 
-if [ -z $1 ]; then
-	## We're at zero
-	deploy_lxcmedia_user
-	deploy_app_config
-	deploy_lxcmedia_ownership
-	supervisord_clean
-	deploy_complete		## Because we don't want it doing stupid.
-else
-	deploy_application_git $1
-	deploy_lxcmedia_ownership
-fi
+echo "[DEPLOY] Deploying lxc-media user:"
+deploy_lxcmedia_user
 
-/usr/bin/supervisorctl reread
-/usr/bin/supervisorctl start $app_name
-/usr/bin/supervisorctl remove deploy
+deploy_application_git reinst
+chown -R $lxcuser:$lxcgroup $app_destdir
+
+sickrage_config_checkdir
+sickrage_deploy_config
+ssl_ssc_create
+sickrage_regenerate_api
+
+echo "[DEPLOY] Changing ownerships for lxc-media..."
+deploy_lxcmedia_ownership
+
+runit_linksv $appname
+
+deploy_complete
